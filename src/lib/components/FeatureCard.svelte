777428babff4a3892a2e8f321e622906074e0e1f
hongbangzhou <joeanddaneil5@gmail.com>
2025-07-02 01:43:16 +0800

    v2

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -16,21 +16,14 @@
 <div
 	class="mt-10 flex flex-col rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 p-1 shadow-lg"
 >
-	<div class={`${color} relative h-auto w-full rounded-lg rounded-b-none`}>
+	<div
+		class={`${color} relative flex aspect-[3/2] w-full flex-col items-center justify-center gap-4 rounded-lg rounded-b-none p-4`}
+	>
 		<div
 			class="absolute inset-0 animate-pulse rounded-lg rounded-b-none border-2 border-pink-500"
 		></div>
-		<img
-			src="https://dummyimage.com/600x400/000/fff&text=joeban"
-			alt="1"
-			class="rounded-lg opacity-5"
-		/>
-		<div
-			class="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4"
-		>
-			<span class="text-4xl">{icon}</span>
-			<span class="digital-font text-sm text-white">{title}</span>
-		</div>
+		<span class="text-4xl">{icon}</span>
+		<span class="digital-font text-sm text-white">{title}</span>
 	</div>
 	<div class="relative rounded-lg rounded-t-none bg-gray-900 p-6">
 		<div
