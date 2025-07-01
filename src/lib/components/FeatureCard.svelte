4605363b7029604dd07156e1e7f61f9d2e2282c8
hongbangzhou <joeanddaneil5@gmail.com>
2025-07-02 01:50:51 +0800

    neonGlowDiv

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -13,22 +13,28 @@
 	}: Props = $props();
 </script>
 
+{#snippet neonGlowDiv(roundedNon: 'top' | 'bottom' | 'all' = 'all')}
+	{#if roundedNon === 'top'}
+		<div class="absolute inset-0 animate-pulse rounded-t-lg border-2 border-pink-500"></div>
+	{:else if roundedNon === 'bottom'}
+		<div class="absolute inset-0 animate-pulse rounded-b-lg border-2 border-pink-500"></div>
+	{:else}
+		<div class="absolute inset-0 animate-pulse rounded-lg border-2 border-pink-500"></div>
+	{/if}
+{/snippet}
+
 <div
 	class="mt-10 flex flex-col rounded-xl bg-gradient-to-r from-pink-500 to-purple-500 p-1 shadow-lg"
 >
 	<div
-		class={`${color} relative flex aspect-[3/2] w-full flex-col items-center justify-center gap-4 rounded-lg rounded-b-none p-4`}
+		class={`${color} relative flex aspect-[3/2] w-full flex-col items-center justify-center gap-4 rounded-t-lg p-4`}
 	>
-		<div
-			class="absolute inset-0 animate-pulse rounded-lg rounded-b-none border-2 border-pink-500"
-		></div>
+		{@render neonGlowDiv('top')}
 		<span class="text-4xl">{icon}</span>
 		<span class="digital-font text-sm text-white">{title}</span>
 	</div>
-	<div class="relative rounded-lg rounded-t-none bg-gray-900 p-6">
-		<div
-			class="absolute inset-0 animate-pulse rounded-lg rounded-t-none border-2 border-pink-500"
-		></div>
+	<div class="ne relative rounded-b-lg bg-gray-900 p-6">
+		{@render neonGlowDiv('bottom')}
 		<h2 class="relative text-xl font-semibold text-white">{title}</h2>
 		<p class="relative mt-2 text-gray-400">{subtitle}</p>
 	</div>
